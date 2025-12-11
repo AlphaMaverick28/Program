ff19568b4b1a7e0e03ab084cf95c5f7cd46380e2
AlphaMaverick28 <atharvkoli47@gmail.com>
2025-12-11 10:01:39 +0530

    commited

--- Main.java
+++ Main.java
@@ -1,49 +1,70 @@
-class Student {
-    // Data members
+// Parent class
+class Person {
     private String name;
     private int age;
 
     // Constructor
-    public Student(String name, int age) {
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    // Getter for name
+    // Getters
     public String getName() {
         return name;
     }
 
-    // Setter for name
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    // Getter for age
     public int getAge() {
         return age;
     }
 
-    // Setter for age
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public void setAge(int age) {
         this.age = age;
     }
 }
 
+// Child class inheriting Person
+class Student extends Person {
+    private String rollNo;
+
+    // Constructor (calls parent constructor using super)
+    public Student(String name, int age, String rollNo) {
+        super(name, age);   // calling Person constructor
+        this.rollNo = rollNo;
+    }
+
+    // Getter & Setter
+    public String getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(String rollNo) {
+        this.rollNo = rollNo;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        // Creating an object
-        Student s = new Student("Atharv", 21);
+        // Create Student object
+        Student s = new Student("Atharv", 21, "AIML123");
 
-        // Using getters
+        // Access inherited and own data
         System.out.println("Name: " + s.getName());
         System.out.println("Age: " + s.getAge());
+        System.out.println("Roll No: " + s.getRollNo());
 
-        // Using setters
+        // Update values
         s.setName("Avi");
         s.setAge(22);
+        s.setRollNo("AIML999");
 
         System.out.println("Updated Name: " + s.getName());
         System.out.println("Updated Age: " + s.getAge());
+        System.out.println("Updated Roll No: " + s.getRollNo());
     }
 }
