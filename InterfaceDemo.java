bdab9f492a44497fa3ec54e3e2bba38e98cb07a9
AlphaMaverick28 <atharvkoli47@gmail.com>
2025-12-11 10:12:23 +0530

    commited

--- InterfaceDemo.java
+++ InterfaceDemo.java
@@ -1,47 +1,78 @@
 // Interface
 interface Animal {
-    void sound();   // abstract method
-    void eat();     // abstract method
+    void sound();
+    void eat();
+
+    // New default method added
+    default void info() {
+        System.out.println("This is an animal from the Animal interface.");
+    }
 }
 
-// Class implementing interface
+// Updated Dog class
 class Dog implements Animal {
 
     @Override
     public void sound() {
-        System.out.println("Dog barks");
+        System.out.println("Dog: Woof Woof!");
     }
 
     @Override
     public void eat() {
-        System.out.println("Dog eats food");
+        System.out.println("Dog eats bones.");
     }
 }
 
-// Another class implementing interface
+// Updated Cat class
 class Cat implements Animal {
 
     @Override
     public void sound() {
-        System.out.println("Cat meows");
+        System.out.println("Cat: Meow Meow!");
     }
 
     @Override
     public void eat() {
-        System.out.println("Cat drinks milk");
+        System.out.println("Cat drinks milk.");
     }
 }
 
-// Main class (file name must match this class)
+// New class added
+class Cow implements Animal {
+
+    @Override
+    public void sound() {
+        System.out.println("Cow: Moo Moo!");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Cow eats grass.");
+    }
+}
+
+// Main class
 public class InterfaceDemo {
     public static void main(String[] args) {
+
         Animal a1 = new Dog();
         Animal a2 = new Cat();
+        Animal a3 = new Cow();  // new object
 
+        a1.info();
         a1.sound();
         a1.eat();
 
+        System.out.println();
+
+        a2.info();
         a2.sound();
         a2.eat();
+
+        System.out.println();
+
+        a3.info();
+        a3.sound();
+        a3.eat();
     }
 }
